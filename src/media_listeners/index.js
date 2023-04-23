702203451d9b9f02e9b7cc19d45605154f741241
Julio Marin <jotacemarin@gmail.com>
2023-04-22 22:15:12 -0500

    add bug fix to medialistener/index.js

--- src/media_listeners/index.js
+++ src/media_listeners/index.js
@@ -1,8 +1,22 @@
 "use strict";
 
+const fs = require("fs");
+
 module.exports = async ({ context, args, bot }) => {
   try {
     const [command] = args;
+
+    if (!String(command).charAt(0).includes("/")) {
+      return null;
+    }
+
+    const file = fs
+      .readdirSync(`${__dirname}`)
+      .find((file) => `/${file}`.includes(command));
+    if (!file) {
+      return null;
+    }
+
     const commandScript = require(`.${command}.js`);
     await commandScript.execute({ context, args, bot });
   } catch (error) {
